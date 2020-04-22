2e4719c333b2aa9007f87582ec1f58eb47ef6a13
ayaanzhaque <ayaanzhaque@gmail.com>
2020-04-22 15:59:19 -0700

    fined tuned VGG Model for better predictions

---
+++ VGG-V2.py
@@ -0,0 +1,226 @@
+import os
+import glob
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from collections import Counter
+import cv2
+from concurrent import futures
+import threading
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import datetime
+# % matplotlib inline
+
+base_dir = os.path.join('./cell_images/cell_images/')
+
+print(base_dir)
+
+infected_dir = os.path.join(base_dir + 'Parasitized')
+healthy_dir = os.path.join(base_dir +'Uninfected')
+
+infected_files = glob.glob(infected_dir + '/*.png')
+healthy_files = glob.glob(healthy_dir + '/*.png')
+print(len(infected_files), len(healthy_files))
+
+np.random.seed(42)
+
+files_df = pd.DataFrame({
+    'filename': infected_files + healthy_files,
+    'label': ['malaria'] * len(infected_files) + ['healthy'] * len(healthy_files)
+}).sample(frac=1, random_state=42).reset_index(drop=True)
+
+print(files_df.head())
+
+train_files, test_files, train_labels, test_labels = train_test_split(files_df['filename'].values,
+                                                                      files_df['label'].values,
+                                                                      test_size=0.3, random_state=42)
+train_files, val_files, train_labels, val_labels = train_test_split(train_files,
+                                                                    train_labels,
+                                                                    test_size=0.1, random_state=42)
+
+print(train_files.shape, val_files.shape, test_files.shape)
+print('Train:', Counter(train_labels), '\nVal:', Counter(val_labels), '\nTest:', Counter(test_labels))
+
+def get_img_shape_parallel(idx, img, total_imgs):
+    if idx % 5000 == 0 or idx == (total_imgs - 1):
+        print('{}: working on img num: {}'.format(threading.current_thread().name,
+                                                  idx))
+    return cv2.imread(img).shape
+
+ex = futures.ThreadPoolExecutor(max_workers=None)
+data_inp = [(idx, img, len(train_files)) for idx, img in enumerate(train_files)]
+print('Starting Img shape computation:')
+train_img_dims_map = ex.map(get_img_shape_parallel,
+                            [record[0] for record in data_inp],
+                            [record[1] for record in data_inp],
+                            [record[2] for record in data_inp])
+train_img_dims = list(train_img_dims_map)
+print('Min Dimensions:', np.min(train_img_dims, axis=0))
+print('Avg Dimensions:', np.mean(train_img_dims, axis=0))
+print('Median Dimensions:', np.median(train_img_dims, axis=0))
+print('Max Dimensions:', np.max(train_img_dims, axis=0))
+
+IMG_DIMS = (125, 125)
+
+def get_img_data_parallel(idx, img, total_imgs):
+    if idx % 5000 == 0 or idx == (total_imgs - 1):
+        print('{}: working on img num: {}'.format(threading.current_thread().name,
+                                                  idx))
+    img = cv2.imread(img)
+    img = cv2.resize(img, dsize=IMG_DIMS,
+                     interpolation=cv2.INTER_CUBIC)
+    img = np.array(img, dtype=np.float32)
+    return img
+
+ex = futures.ThreadPoolExecutor(max_workers=None)
+train_data_inp = [(idx, img, len(train_files)) for idx, img in enumerate(train_files)]
+val_data_inp = [(idx, img, len(val_files)) for idx, img in enumerate(val_files)]
+test_data_inp = [(idx, img, len(test_files)) for idx, img in enumerate(test_files)]
+
+print('Loading Train Images:')
+train_data_map = ex.map(get_img_data_parallel,
+                        [record[0] for record in train_data_inp],
+                        [record[1] for record in train_data_inp],
+                        [record[2] for record in train_data_inp])
+train_data = np.array(list(train_data_map))
+
+print('\nLoading Validation Images:')
+val_data_map = ex.map(get_img_data_parallel,
+                        [record[0] for record in val_data_inp],
+                        [record[1] for record in val_data_inp],
+                        [record[2] for record in val_data_inp])
+val_data = np.array(list(val_data_map))
+
+print('\nLoading Test Images:')
+test_data_map = ex.map(get_img_data_parallel,
+                        [record[0] for record in test_data_inp],
+                        [record[1] for record in test_data_inp],
+                        [record[2] for record in test_data_inp])
+test_data = np.array(list(test_data_map))
+
+print(train_data.shape, val_data.shape, test_data.shape)
+
+plt.figure(1 , figsize = (8 , 8))
+n = 0
+for i in range(16):
+    n += 1
+    r = np.random.randint(0 , train_data.shape[0] , 1)
+    plt.subplot(4 , 4 , n)
+    plt.subplots_adjust(hspace = 0.5 , wspace = 0.5)
+    plt.imshow(train_data[r[0]]/255.)
+    plt.title('{}'.format(train_labels[r[0]]))
+    plt.xticks([]) , plt.yticks([])
+# plt.show()
+
+BATCH_SIZE = 64
+NUM_CLASSES = 2
+EPOCHS = 25
+INPUT_SHAPE = (125, 125, 3)
+
+train_imgs_scaled = train_data / 255.
+val_imgs_scaled = val_data / 255.
+
+# encode text category labels
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+le.fit(train_labels)
+train_labels_enc = le.transform(train_labels)
+val_labels_enc = le.transform(val_labels)
+
+print(train_labels[:6], train_labels_enc[:6])
+
+logdir = os.path.join('.',
+                      datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255,
+                                                                zoom_range=0.05,
+                                                                rotation_range=25,
+                                                                width_shift_range=0.05,
+                                                                height_shift_range=0.05,
+                                                                shear_range=0.05, horizontal_flip=True,
+                                                                fill_mode='nearest')
+
+val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+# build image augmentation generators
+train_generator = train_datagen.flow(train_data, train_labels_enc, batch_size=BATCH_SIZE, shuffle=True)
+val_generator = val_datagen.flow(val_data, val_labels_enc, batch_size=BATCH_SIZE, shuffle=False)
+
+img_id = 0
+sample_generator = train_datagen.flow(train_data[img_id:img_id+1], train_labels[img_id:img_id+1],
+                                      batch_size=1)
+sample = [next(sample_generator) for i in range(0,5)]
+fig, ax = plt.subplots(1,5, figsize=(16, 6))
+print('Labels:', [item[1][0] for item in sample])
+l = [ax[i].imshow(sample[i][0][0]) for i in range(0,5)]
+
+
+vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet',
+                                        input_shape=INPUT_SHAPE)
+# Freeze the layers
+vgg.trainable = True
+
+set_trainable = False
+for layer in vgg.layers:
+    if layer.name in ['block5_conv1', 'block4_conv1']:
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+base_vgg = vgg
+base_out = base_vgg.output
+pool_out = tf.keras.layers.Flatten()(base_out)
+hidden1 = tf.keras.layers.Dense(512, activation='relu')(pool_out)
+drop1 = tf.keras.layers.Dropout(rate=0.3)(hidden1)
+hidden2 = tf.keras.layers.Dense(512, activation='relu')(drop1)
+drop2 = tf.keras.layers.Dropout(rate=0.3)(hidden2)
+
+out = tf.keras.layers.Dense(1, activation='sigmoid')(drop2)
+
+model = tf.keras.Model(inputs=base_vgg.input, outputs=out)
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=1e-5),
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+print("Total Layers:", len(model.layers))
+print("Total trainable layers:", sum([1 for l in model.layers if l.trainable]))
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                              patience=2, min_lr=0.000001)
+
+callbacks = [reduce_lr, tensorboard_callback]
+train_steps_per_epoch = train_generator.n // train_generator.batch_size
+val_steps_per_epoch = val_generator.n // val_generator.batch_size
+history = model.fit_generator(train_generator, steps_per_epoch=train_steps_per_epoch, epochs=EPOCHS,
+                              validation_data=val_generator, validation_steps=val_steps_per_epoch,
+                              verbose=1)
+
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
+t = f.suptitle('Basic CNN Performance', fontsize=12)
+f.subplots_adjust(top=0.85, wspace=0.3)
+
+max_epoch = len(history.history['accuracy'])+1
+epoch_list = list(range(1,max_epoch))
+ax1.plot(epoch_list, history.history['accuracy'], label='Train Accuracy')
+ax1.plot(epoch_list, history.history['val_accuracy'], label='Validation Accuracy')
+ax1.set_xticks(np.arange(1, max_epoch, 5))
+ax1.set_ylabel('Accuracy Value')
+ax1.set_xlabel('Epoch')
+ax1.set_title('Accuracy')
+l1 = ax1.legend(loc="best")
+
+ax2.plot(epoch_list, history.history['loss'], label='Train Loss')
+ax2.plot(epoch_list, history.history['val_loss'], label='Validation Loss')
+ax2.set_xticks(np.arange(1, max_epoch, 5))
+ax2.set_ylabel('Loss Value')
+ax2.set_xlabel('Epoch')
+ax2.set_title('Loss')
+l2 = ax2.legend(loc="best")
+
+model.save('vggFineTunedWeights.h5')
